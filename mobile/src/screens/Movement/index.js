bcf39ced30d54b14202ae721ba49fd01de1dd906
Melquias Ribeiro <melquias10.mr@gmail.com>
2020-09-09 17:39:23 -0300

    feat: history screen

--- mobile/src/screens/Movement/index.js
+++ mobile/src/screens/Movement/index.js
@@ -1,13 +1,181 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+    Foundation,
+    FontAwesome5,
+    Feather,
+    MaterialIcons,
+} from '@expo/vector-icons';
 
-import { Container } from './styles';
+import {
+    Container,
+    Hearder,
+    IconHeader,
+    Title,
+    UpLineContainer,
+    NewActivity,
+    RevenueContainer,
+    RevenuesTitle,
+    RevenuesValue,
+    Line,
+    RevenuesLegend,
+    HistoryContainer,
+    HistoryTitle,
+    ActivityContainer,
+    ActivityMainInfoContainer,
+    IconContainer,
+    TextInfoActivity,
+    Info,
+    Activity,
+    ActivityValue,
+} from './styles';
 
 const Movement = () => {
+    const activities = [
+        {
+            id: 1,
+            saleItems: [
+                { id: 2, salePrice: 10 },
+                { id: 3, salePrice: 10 },
+                { id: 4, salePrice: 50 },
+            ],
+            payment_type: 'Cratão de Crédito',
+            note: 'Rosa do deserto',
+            amount: 70,
+            sale: true,
+            date: 'Dom 20/08/20',
+        },
+        {
+            id: 2,
+            saleItems: [
+                { id: 2, salePrice: 10 },
+                { id: 3, salePrice: 10 },
+                { id: 4, salePrice: 50 },
+            ],
+            payment_type: 'Cratão de Crédito',
+            note: 'Terra e vasos',
+            amount: 70,
+            sale: true,
+            date: 'Dom 20/08/20',
+        },
+        {
+            id: 3,
+            saleItems: [
+                { id: 2, salePrice: 10 },
+                { id: 3, salePrice: 10 },
+                { id: 4, salePrice: 50 },
+            ],
+            payment_type: 'Cratão de Crédito',
+            note: 'Posto de gasolina',
+            amount: 70,
+            sale: false,
+            date: 'Dom 20/08/20',
+        },
+        {
+            id: 4,
+            saleItems: [
+                { id: 2, salePrice: 10 },
+                { id: 3, salePrice: 10 },
+                { id: 4, salePrice: 50 },
+            ],
+            payment_type: 'Cratão de Crédito',
+            note: 'Posto de gasolina',
+            amount: 70,
+            sale: false,
+            date: 'Dom 20/08/20',
+        },
+        {
+            id: 5,
+            saleItems: [
+                { id: 2, salePrice: 10 },
+                { id: 3, salePrice: 10 },
+                { id: 4, salePrice: 50 },
+            ],
+            payment_type: 'Cratão de Crédito',
+            note: 'Posto de gasolina',
+            amount: 70,
+            sale: true,
+            date: 'Dom 20/08/20',
+        },
+        {
+            id: 6,
+            saleItems: [
+                { id: 2, salePrice: 10 },
+                { id: 3, salePrice: 10 },
+                { id: 4, salePrice: 50 },
+            ],
+            payment_type: 'Cratão de Crédito',
+            note: 'Posto de gasolina',
+            amount: 70,
+            sale: false,
+            date: 'Dom 20/08/20',
+        },
+        {
+            id: 7,
+            saleItems: [
+                { id: 2, salePrice: 10 },
+                { id: 3, salePrice: 10 },
+                { id: 4, salePrice: 50 },
+            ],
+            payment_type: 'Cratão de Crédito',
+            note: 'Posto de gasolina',
+            amount: 70,
+            sale: true,
+            date: 'Dom 20/08/20',
+        },
+    ];
     return (
-        <View>
-            <Text>movment</Text>
-        </View>
+        <Container>
+            <Hearder>
+                <IconHeader>
+                    <Foundation name="refresh" size={28} color="#ffffff" />
+                </IconHeader>
+                <Title>Entradas e saídas</Title>
+                <UpLineContainer>
+                    <RevenueContainer>
+                        <RevenuesTitle>Faturamento </RevenuesTitle>
+                        <RevenuesValue>R$ 5.000,00</RevenuesValue>
+                    </RevenueContainer>
+                    <NewActivity>
+                        <FontAwesome5 name="plus" size={16} color="#fafafa" />
+                    </NewActivity>
+                </UpLineContainer>
+                <Line />
+                <RevenuesLegend>Referente ao mês vigente</RevenuesLegend>
+            </Hearder>
+            <HistoryContainer>
+                <HistoryTitle>Histórico</HistoryTitle>
+                {activities.map((activity) => (
+                    <ActivityContainer key={activity.id}>
+                        <ActivityMainInfoContainer>
+                            <IconContainer>
+                                {activity.sale ? (
+                                    <Feather
+                                        name="package"
+                                        size={24}
+                                        color="#ffffff"
+                                    />
+                                ) : (
+                                    <MaterialIcons
+                                        name="money-off"
+                                        size={24}
+                                        color="#ffffff"
+                                    />
+                                )}
+                            </IconContainer>
+                            <TextInfoActivity>
+                                <Activity>{activity.note} </Activity>
+                                <Info>{activity.payment_type}</Info>
+                                <Info>{activity.date}</Info>
+                            </TextInfoActivity>
+                        </ActivityMainInfoContainer>
+                        <ActivityValue sale={activity.sale}>
+                            R${activity.amount}
+                        </ActivityValue>
+                    </ActivityContainer>
+                ))}
+            </HistoryContainer>
+        </Container>
     );
 };
 
--- mobile/src/screens/Movement/styles.js
+++ mobile/src/screens/Movement/styles.js
@@ -1,3 +1,124 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+    flex: 1;
+`;
+export const Hearder = styled.View`
+    background: #ff5057;
+    padding: 44px 20px 0px 20px;
+`;
+
+export const IconHeader = styled.TouchableOpacity`
+    align-items: flex-end;
+    margin-right: 15px;
+`;
+
+export const Title = styled.Text`
+    text-align: center;
+    font-size: 24px;
+    font-weight: bold;
+    color: #ffffff;
+`;
+export const UpLineContainer = styled.View`
+    flex-direction: row;
+    margin-top: 32px;
+    align-items: center;
+    justify-content: space-between;
+`;
+export const NewActivity = styled.TouchableOpacity`
+    width: 48px;
+    height: 48px;
+    background: #b53e43;
+    border-radius: 8px;
+    align-items: center;
+    justify-content: center;
+`;
+export const RevenueContainer = styled.View``;
+export const RevenuesTitle = styled.Text`
+    font-weight: bold;
+    font-size: 14px;
+    line-height: 16px;
+    color: #ffffff;
+`;
+export const RevenuesValue = styled.Text`
+    margin-top: 4px;
+    font-weight: bold;
+    margin-left: 8px;
+    font-size: 24px;
+    line-height: 36px;
+    color: #ffffff;
+`;
+
+export const Line = styled.View`
+    height: 0px;
+    opacity: 0.15;
+    border: 1px solid #ffffff;
+    margin: 16px 0 8px 0;
+`;
+export const RevenuesLegend = styled.Text`
+    font-weight: bold;
+    font-size: 14px;
+    line-height: 16px;
+    color: #ffffff;
+    margin: 8px 0 40px 0;
+`;
+
+export const HistoryContainer = styled.ScrollView.attrs(() => ({
+    showsVerticalScrollIndicator: false,
+}))`
+    margin: 20px 20px 0px 20px;
+`;
+export const HistoryActivityContainer = styled.View``;
+
+export const HistoryTitle = styled.Text`
+    font-weight: bold;
+    font-size: 18px;
+    line-height: 21px;
+    color: #1a2f7b;
+`;
+
+export const ActivityContainer = styled.View`
+    flex-direction: row;
+    justify-content: space-around;
+    height: 72px;
+    background: #ffffff;
+    border-radius: 15px;
+    align-items: center;
+    margin: 8px 0 8px 0;
+`;
+export const ActivityMainInfoContainer = styled.View`
+    flex-direction: row;
+    flex: 1;
+    margin-left: 8px;
+`;
+export const IconContainer = styled.View`
+    width: 56.82px;
+    height: 52.52px;
+    background: #ff5057;
+    border-radius: 21px;
+    align-items: center;
+    justify-content: center;
+`;
+export const TextInfoActivity = styled.View`
+    margin-left: 8px;
+    justify-content: center;
+`;
+export const Activity = styled.Text`
+    font-weight: bold;
+    font-size: 18px;
+    line-height: 21px;
+    color: #1a2f7b;
+`;
+export const Info = styled.Text`
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 16px;
+    color: #a0a0a0;
+    margin-top: 4px;
+`;
+export const ActivityValue = styled.Text`
+    font-size: 18px;
+    line-height: 21px;
+    color: ${(props) => (props.sale ? '#5CBC71' : '#ff1c25')};
+    margin-right: 8px;
+`;
